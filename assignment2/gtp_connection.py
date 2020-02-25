b8c429ec70bdf51f6acad172428bfe8e0dabb874
aapaetsch <aapaetsch@gmail.com>
2020-02-24 19:11:42 -0700

    test

--- assignment2/gtp_connection.py
+++ assignment2/gtp_connection.py
@@ -43,6 +43,7 @@ class GtpConnection():
         self.go_engine = go_engine
         self.board = board
         self.time_limit = 1
+        self.originalPlayer = None
         self.commands = {
             "protocol_version": self.protocol_version_cmd,
             "quit": self.quit_cmd,
@@ -56,8 +57,6 @@ class GtpConnection():
             "genmove": self.genmove_cmd,
             "list_commands": self.list_commands_cmd,
             "play": self.play_cmd,
-            "solve": self.solve_cmd,
-            "timelimit": self.time_limit_cmd,
             "legal_moves": self.legal_moves_cmd,
             "gogui-rules_game_id": self.gogui_rules_game_id_cmd,
             "gogui-rules_board_size": self.gogui_rules_board_size_cmd,
@@ -65,9 +64,9 @@ class GtpConnection():
             "gogui-rules_side_to_move": self.gogui_rules_side_to_move_cmd,
             "gogui-rules_board": self.gogui_rules_board_cmd,
             "gogui-rules_final_result": self.gogui_rules_final_result_cmd,
-            "gogui-analyze_commands": self.gogui_analyze_cmd
-            
-            
+            "gogui-analyze_commands": self.gogui_analyze_cmd,
+            "timelimit": self.time_limit_cmd,
+            "solve": self.solve_cmd
         }
 
         # used for argument checking
@@ -78,10 +77,9 @@ class GtpConnection():
             "komi": (1, 'Usage: komi FLOAT'),
             "known_command": (1, 'Usage: known_command CMD_NAME'),
             "genmove": (1, 'Usage: genmove {w,b}'),
-            "timelimit":(1, 'Usage: timelimit INT'),
             "play": (2, 'Usage: play {b,w} MOVE'),
-            "legal_moves": (1, 'Usage: legal_moves {w,b}')
-            
+            "legal_moves": (1, 'Usage: legal_moves {w,b}'),
+            "timelimit":(1, 'Usage: timelimit INT')
         }
     
     def write(self, data):
