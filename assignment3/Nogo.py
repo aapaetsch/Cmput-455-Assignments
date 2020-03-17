59875acb8f8e044189a5f122d25651942abfe114
Aidan Paetsch <aapaetsch@gmail.com>
2020-03-17 00:42:20 -0600

    pattern not working

--- assignment3/Nogo.py
+++ assignment3/Nogo.py
@@ -3,14 +3,23 @@
 # Set the path to your python3 above
 
 from gtp_connection_nogo3 import GtpConnectionNoGo3
-from board_util import GoBoardUtil, BLACK, WHITE, EMPTY
+from board_util import GoBoardUtil, BLACK, WHITE, EMPTY, BORDER
 from simple_board import SimpleGoBoard
 from legalMoveGen import getLegalMoves
 import sys
 import ucb
 import numpy as np
 import random
+from pattern_util import PatternUtil
+from pattern import pat3set
 
+def openFile(fileName):
+    weights = {}
+    with open(fileName, 'r') as f:
+        for line in f:
+            item = line.split(' ')
+            weights[int(item[0])] = float(item[1])
+    return weights
 
 class Nogo():
     def __init__(self):
@@ -28,24 +37,22 @@ class Nogo():
 
     def randomSimulation(self, state, move, toplay):
         tempState = state.copy()
-        #<---Play 1 game to the end from here--->
-        # self.quickPlayMove(tempState, move, toplay)
         tempState.play_move(move, toplay)
         while True:
-
             cp = tempState.current_player
-            # legalMoves = getLegalMoves(tempState, cp)
-            legalMoves = self.generateLegalMoves(tempState, cp)
-            if self.isTerminal(legalMoves):
-                return cp
-
-            random.shuffle(legalMoves)
-            move = legalMoves.pop()
-            # self.quickPlayMove(tempState, move, cp)
+            move = self.randomMoveGen(tempState, cp)
+            if move == None:
+                return self.evaluate(cp)
             tempState.play_move(move, cp)
 
-    def patternSimulation(self, state, move, toplay):
-        pass
+    def randomMoveGen(self, state, player):
+        moves = state.get_empty_points()
+        np.random.shuffle(moves)
+        for move in moves:
+            legal = state.is_legal(move, player)
+            if legal:
+                return move
+        return None
 
     def quickPlayMove(self, state, move, player):
         state.board[move] = player
@@ -56,7 +63,6 @@ class Nogo():
         cp = state.current_player
         state.current_player = WHITE + BLACK - cp
 
-
     def simulate(self, state, move, toplay):
         if self.policy == 'random':
             return self.randomSimulation(state, move, toplay)
@@ -64,16 +70,15 @@ class Nogo():
             return self.patternSimulation(state, move, toplay)
 
 
-    def simulateMove(self, board, move, toplay):
+    def simulateMove(self, state, move, toplay):
         #<---simulation for a given move--->
         wins = 0 
+
         for _ in range(self.num_sim):
-            result = self.simulate(board, move, toplay)
+            result = self.simulate(state, move, toplay)
             if result == toplay:
                 wins += 1
         return wins
-
-
 
     def getMoves(self, state, color):
         self.originalPlayer = color
@@ -85,13 +90,16 @@ class Nogo():
         if not legalMoves:
             return None
 
+        if self.policy == 'pattern':
+            self.weights = openFile('weights')
+
         if self.selection == 'rr':
             #<---Do a round robin selection--->
             print('White' if color == WHITE else 'black')
             for move in legalMoves:
                 wins = self.simulateMove(gameState, move, color)
-                probs[move] = round(wins/(len(legalMoves)*self.num_sim),3)
-
+                # probs[move] = round(wins/(len(legalMoves)*self.num_sim),3)
+                probs[move] = wins
 
         else:
             #<---Do a UCB Selection--->
@@ -105,14 +113,30 @@ class Nogo():
                     stats[moveIndex][0] += 1
                 stats[moveIndex][1] += 1
 
+        self.weights = {}
+        if self.selection == 'rr':
+            if self.policy == 'random':
+                for key in probs.keys():
+                    probs[key] = round(self.num_sim / (self.num_sim * len(legalMoves)),3)
+            else:
+                probSum = 0 
+                for key in probs.keys():
+                    x = wins/len(legalMoves)*self.num_sim
+                    probs[key] = x
+                    probSum += x
+                for key in probs.keys():
+                    probs[key] = probs[key]/probSum
 
-
+        #<---NEED TO ADD THE THING.... probs for rr random forces even distro... need to send a valid dict to get a valid genmove rr random answer
         return probs
         
     def isTerminal(self, remainingMoves):
         if len(remainingMoves) == 0:
             return True
         return False
+
+    def evaluate(self, cp):
+        return BLACK + WHITE - cp
 
     def generateLegalMoves(self, gameState, color):
         ePts = gameState.get_empty_points()
@@ -121,6 +145,58 @@ class Nogo():
             if gameState.is_legal(pt, color):
                 moves.append(pt)
         return moves
+
+    def getWeight(self, state,  toplay, move):
+        pattern = PatternUtil.neighborhood_33(state, move)
+        pattern = pattern[:4] + pattern[5:]#Get rid of point p 
+        addy = 0 
+        index = 0 
+        for i in pattern:
+            if i == 'X':
+                addy += toplay * pow(4, index)
+            
+            elif i == 'x':
+                addy += (BLACK + WHITE - toplay) * pow(4, index)
+                
+            elif i == '.':
+                addy += EMPTY * pow(4, index)
+
+            elif i == ' ':
+                addy += BORDER * pow(4, index)
+            index += 1 
+        return self.weights[addy]        
+
+    def patternSimulation(self, state, move, toplay):
+        tempState = state.copy()
+        tempState.play_move(move, toplay)
+
+        while True:
+            cp = tempState.current_player
+            legalMoves = self.generateLegalMoves(tempState, cp)
+            if self.isTerminal(legalMoves):
+                return self.evaluate(cp)
+
+            # #<---generate pattern moves--->
+            # pattern_checking_set = tempState.last_moves_empty_neighbors()
+            # moves = []
+            # for p in pattern_checking_set:
+            #     if (PatternUtil.neighborhood_33(tempState, p) in pat3set):
+            #         assert p not in moves
+            #         assert board.board[p] == EMPTY
+            #         moves.append(p)
+            # #<---Move by weight--->
+            #<---Probably doing this wrong...
+            weightedMax = 0 
+            weightedMove = None
+            for move in legalMoves:
+                # weightedMoves[move] = self.getWeight(tempState, cp, move) 
+                weight = self.getWeight(tempState, cp, move)
+                if weight >= weightedMax:
+                    weightedMax = weight
+                    weightedMove = move
+
+            tempState.play_move(weightedMove, cp)
+
 
 
 # def byPercentage(pair):
